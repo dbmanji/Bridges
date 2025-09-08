7eefda4c41e67f7ec2c3c3bedc3fb284405c559e
sinwolfe <mqhakimi@pm.me>
2025-09-08 18:41:30 +0800

    chore: add comment to explain current circumstances

--- client/src/routes/login/+page.server.ts
+++ client/src/routes/login/+page.server.ts
@@ -2,6 +2,7 @@ import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import { ClientResponseError } from 'pocketbase';
 
+// this is all placeholder for now. we need to setup pocketbase properly. initial setup seems absolutely wrong to me - hakimi
 export const actions: Actions = {
 	login: async ({ request, locals }) => {
 		const formData = await request.formData();
